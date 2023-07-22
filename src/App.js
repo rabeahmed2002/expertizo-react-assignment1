20982ce059170c5750a93a69f5651474e5c86e55
rabeahmed2002 <mrabe2024@gmail.com>
2023-07-22 09:53:59 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -2,21 +2,50 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const name = "Hello World";
+
+  const obj = { message: "Welcome to expertizo" }
+
+  const data = ['We', 'are', 'United']
+
+  const list = [{name: "Hello World 1"}, {name: "Hello World 2"}, {name: "Hello World 3"}] //Render this using the map function
+
+  const complex = [{company: 'XYZ', jobs: ['JavaScript,', 'React']}, {company: 'ABC', jobs: ['Angular', 'Ionic']}] //Show these in a Table
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+            <header className="App-header">
+
+      <h1>{name}</h1>
+
+      <h1>{obj.message}</h1>
+
+      <h1>{data[0]}</h1>
+      <h1>{data[1]}</h1>
+      <h1>{data[2]}</h1>
+
+      {list.map((item,index)=>{
+        return <h1>{item.name}</h1>
+      })}
+
+
+<table>
+        <thead>
+          <tr>
+            <th>Company</th>
+            <th>Jobs</th>
+          </tr>
+        </thead>
+        <tbody>
+          {complex.map((item, index) => (
+            <tr key={index}>
+              <td>{item.company}</td>
+              <td>{item.jobs.join(', ')}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
       </header>
     </div>
   );
